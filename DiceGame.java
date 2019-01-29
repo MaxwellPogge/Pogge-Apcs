28818df9fa7f877331af0ef6ff108060037446af
Maxwell Pogge <Pogge.Maxw2022@ssusd.org>
2019-01-29 06:50:36 -0800

    1st checkpoint

---
+++ DiceGame.java
@@ -0,0 +1,35 @@
+class DiceGame{
+   // public static void main (String[] args){
+
+     //   int D4 = getD4();
+      //  int D12 = getD12();
+        //int D24 = getD24();
+    //}
+    
+    //public static int getD4(){
+        //int D4=(int)(Math.random() * 4) + 1;
+        //System.out.println(D4);
+        //return D4;
+       
+    //}
+
+    //public static int getD12(){
+        //int D12=(int)(Math.random() * 12) + 1;
+        //System.out.println(D12);
+        //return D12;
+
+    }  
+
+    //public static int getD24(){
+      //  int D24=(int)(Math.random() * 24) + 1; 
+        //System.out.println(D24);
+        //return D24;    
+
+    int x=0;
+    while (x<300000000) {
+        x++
+
+    } 
+
+}
+
